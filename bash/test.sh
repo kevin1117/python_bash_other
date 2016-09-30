527b6c8412363c32580d5acc011599eae6566b51
Kevin Zhang <zfj321@qq.com>
2016-09-30 21:21:13 +0800

    修改测试shell脚本

--- bash/test.sh
+++ bash/test.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 echo "zfj"
 
+echo '赋值=左右不能有空格'
+
+#遍历目录
+
+cd /Users/kevinzhang
+
+list=`ls`
+for fpath in $list
+do
+ if [ -f $fpath ];then
+    echo "======file";
+ else
+    echo "#####other"
+ fi
+done
+
+if [ -e "/bin/ls" ]; then
+   echo "============="
+fi
+
+#打印日期
+d=`date '+%Y-%m-%d %H:%M:S'`
+echo $d
 
 
